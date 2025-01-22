9ff47ba403aea7b0857dabd8dc668e335497f1b6
abracodeabra <akakiplimo@gmail.com>
2025-01-23 02:25:33 +0300

    Initial Frontend Commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,32 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {
-    extend: {
+  themes: [
+    {
+      themeName: 'light',
+      colorScheme: 'light',
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: '#235264',
+        backgroundPrimary: '#964643',
       },
     },
+    {
+      themeName: 'dark',
+      colorScheme: 'dark',
+      colors: {
+        primary: '#573242',
+        backgroundPrimary: '#1a1a1a',
+      },
+    },
+  ],
+  plugins: [require('rippleui')],
+  rippleui: {
+    defaultStyle: false,
+    removeThemes: ['dark'],
   },
-  plugins: [],
 } satisfies Config;
